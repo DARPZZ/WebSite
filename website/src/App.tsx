c4c4d9b946784cce1ba65bbf351e699f990ec343
Rasmus <rasmustofthermansen@gmail.com>
2024-07-08 23:19:45 +0200

    Added background to its own file

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,7 +3,7 @@ import NavBar from "./NavBarFolder/NavBar";
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadFull } from "tsparticles";
-
+import Background from "./Background";
 function App() {
   const [init, setInit] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -24,93 +24,12 @@ function App() {
 
     window.addEventListener("resize", handleResize);
 
-
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <div className="relative">
-      {init && (
-        <div className={`fixed top-0 left-0 w-full h-full ${isMobile ? '-z-10 bg-gray-300' : ''}`}>
-          {!isMobile && (
-            <Particles
-              id="tsparticles"
-              className="fixed top-0 left-0 w-full h-full -z-10"
-              options={{
-                background: {
-                  color: {
-                    value: "#808080",
-                  },
-                },
-                fpsLimit: 120,
-                interactivity: {
-                  events: {
-                    onClick: {
-                      enable: false,
-                      mode: "push",
-                    },
-                    onHover: {
-                      enable: false,
-                      mode: "repulse",
-                    },
-                    resize: {
-                      enable: true,
-                      delay: 0.5,
-                    },
-                  },
-                  modes: {
-                    push: {
-                      quantity: 4,
-                    },
-                    repulse: {
-                      distance: 200,
-                      duration: 0.4,
-                    },
-                  },
-                },
-                particles: {
-                  number: {
-                    value: 50,
-                    density: {
-                      enable: true,
-                    },
-                  },
-                  color: {
-                    value: "#ffffff",
-                  },
-                  links: {
-                    color: "#ffffff",
-                    distance: 150,
-                    enable: true,
-                    opacity: 0.5,
-                    width: 1,
-                  },
-                  move: {
-                    direction: "none",
-                    enable: true,
-                    outModes: {
-                      default: "bounce",
-                    },
-                    random: false,
-                    speed: 2,
-                    straight: false,
-                  },
-                  opacity: {
-                    value: 0.3,
-                  },
-                  shape: {
-                    type: "circle",
-                  },
-                  size: {
-                    value: { min: 1, max: 5 },
-                  },
-                },
-                detectRetina: true,
-              }}
-            />
-          )}
-        </div>
-      )}
+    <div>
+      <Background />
       <div className="relative z-10">
         <NavBar />
         <Links />
---
+++ website/src/Background.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+
+import React from "react";
+
+function Background() {
+  const [init, setInit] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleResize = () => {
+      setIsMobile(mediaQuery.matches);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="relative">
+      {init && (
+        <div
+          className={`fixed top-0 left-0 w-full h-full ${
+            isMobile ? "-z-10 bg-gray-300" : ""
+          }`}
+        >
+          {!isMobile && (
+            <Particles
+              id="tsparticles"
+              className="fixed top-0 left-0 w-full h-full -z-10"
+              options={{
+                background: {
+                  color: {
+                    value: "#808080",
+                  },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                  events: {
+                    onClick: {
+                      enable: false,
+                      mode: "push",
+                    },
+                    onHover: {
+                      enable: false,
+                      mode: "repulse",
+                    },
+                    resize: {
+                      enable: true,
+                      delay: 0.5,
+                    },
+                  },
+                  modes: {
+                    push: {
+                      quantity: 4,
+                    },
+                    repulse: {
+                      distance: 200,
+                      duration: 0.4,
+                    },
+                  },
+                },
+                particles: {
+                  number: {
+                    value: 50,
+                    density: {
+                      enable: true,
+                    },
+                  },
+                  color: {
+                    value: "#ffffff",
+                  },
+                  links: {
+                    color: "#ffffff",
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.5,
+                    width: 1,
+                  },
+                  move: {
+                    direction: "none",
+                    enable: true,
+                    outModes: {
+                      default: "bounce",
+                    },
+                    random: false,
+                    speed: 2,
+                    straight: false,
+                  },
+                  opacity: {
+                    value: 0.3,
+                  },
+                  shape: {
+                    type: "circle",
+                  },
+                  size: {
+                    value: { min: 1, max: 5 },
+                  },
+                },
+                detectRetina: true,
+              }}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Background;
