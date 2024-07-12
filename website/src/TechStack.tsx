4478a9d604a33b9ff8319bb2759302bafbc9ad45
Rasmus <rasmustofthermansen@gmail.com>
2024-07-12 19:17:34 +0200

    Rotating around

--- website/src/TechStack.tsx
+++ website/src/TechStack.tsx
@@ -1,32 +1,31 @@
-import React from "react";
-import ThreeJSStack from "./ThreeJSStack";
-import reactImg from "./assets/TechnoloPic/react.png";
-import typeScriptImg from "./assets/TechnoloPic/TS.png";
-import cSharpImg from "./assets/TechnoloPic/CSharp.png";
-import javaImg from "./assets/TechnoloPic/java.png";
-import pythonImg from "./assets/TechnoloPic/python.png";
-import githubImg from "./assets/TechnoloPic/github.png"
-import dockerImg from "./assets/TechnoloPic/docker.png"
-import sqlImg from  "./assets/TechnoloPic/sql.png"
+import React from 'react';
+import ThreeJSStack from './ThreeJSStack';
+import reactImg from './assets/TechnoloPic/react.png';
+import typeScriptImg from './assets/TechnoloPic/TS.png';
+import cSharpImg from './assets/TechnoloPic/CSharp.png';
+import javaImg from './assets/TechnoloPic/java.png';
+import pythonImg from './assets/TechnoloPic/python.png';
+import githubImg from './assets/TechnoloPic/github.png';
+import dockerImg from './assets/TechnoloPic/docker.png';
+import sqlImg from './assets/TechnoloPic/sql.png';
+
 const TechStack: React.FC = () => {
   const techStack = [
-    { title: "React", name: "React", iconUrl: reactImg },
-    { title: "TypeScript", name: "TypeScript", iconUrl: typeScriptImg },
-    { title: "C sharp", name: "c#", iconUrl: cSharpImg },
-    { title: "Java", name: "Java", iconUrl: javaImg },
-    { title: "Python", name: "Python", iconUrl: pythonImg },
-    { title: "Github", name: "Github", iconUrl: githubImg },
-    { title: "Docker", name: "Docker", iconUrl: dockerImg },
-    { title: "SQL", name: "SQL", iconUrl: sqlImg },
+    { name: 'React', iconUrl: reactImg },
+    { name: 'TypeScript', iconUrl: typeScriptImg },
+    { name: 'c#', iconUrl: cSharpImg },
+    { name: 'Java', iconUrl: javaImg },
+    { name: 'Python', iconUrl: pythonImg },
+    { name: 'Github', iconUrl: githubImg },
+    { name: 'Docker', iconUrl: dockerImg },
+    { name: 'SQL', iconUrl: sqlImg },
   ];
 
   return (
     <div className="w-full px-5">
-      <h2 className="text-5xl  font-extrabold text-blue-300">Tech Stack</h2>
-      <div className="grid grid-cols-4 gap-4">
-        {techStack.map((item, index) => (
-          <ThreeJSStack key={index} techStack={[item]} title={item.title} />
-        ))}
+      <h2 className="text-5xl font-extrabold text-blue-300">Tech Stack</h2>
+      <div className="">
+        <ThreeJSStack title="Tech Stack" techStack={techStack} />
       </div>
     </div>
   );
--- website/src/ThreeJSStack.tsx
+++ website/src/ThreeJSStack.tsx
@@ -8,78 +8,43 @@ interface ThreeJSStackProps {
 
 const ThreeJSStack: React.FC<ThreeJSStackProps> = ({ title, techStack }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
+  const sceneRef = useRef<THREE.Scene | null>(null);
+  const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
   const iconsRef = useRef<THREE.Mesh[]>([]);
 
   useEffect(() => {
-    let animationId: number;
-    let renderer: THREE.WebGLRenderer | null = null;
-    let scene: THREE.Scene | null = null;
-    let camera: THREE.PerspectiveCamera | null = null;
+    let animationId: number | null = null;
 
     const initializeScene = () => {
-      scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
-      renderer = new THREE.WebGLRenderer({ antialias: true });
-      renderer.setClearColor(0x000000, 0);
+      rendererRef.current = new THREE.WebGLRenderer({ antialias: true });
+      rendererRef.current.setClearColor(0x000000, 0);
+      sceneRef.current = new THREE.Scene();
+      cameraRef.current = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
 
       const ambientLight = new THREE.AmbientLight(0xffffff, 5);
-      scene.add(ambientLight);
+      sceneRef.current.add(ambientLight);
 
       const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
       directionalLight.position.set(5, 5, 5);
-      scene.add(directionalLight);
+      sceneRef.current.add(directionalLight);
 
-      camera.position.set(0, 0, 40);
+      cameraRef.current.position.set(0, 0, 40);
 
-      if (containerRef.current && renderer) {
-        containerRef.current.appendChild(renderer.domElement);
-      }
-
-      // Event listener for context loss
-      renderer.domElement.addEventListener('webglcontextlost', handleContextLost, false);
-      renderer.domElement.addEventListener('webglcontextrestored', handleContextRestored, false);
-    };
-
-    const handleContextLost = (event: Event) => {
-      event.preventDefault();
-      cancelAnimationFrame(animationId);
-      cleanup();
-    };
-
-    const handleContextRestored = () => {
-      initializeScene();
-      loadIcons();
-      resizeRendererToDisplaySize();
-      animate();
-    };
-
-    const resizeRendererToDisplaySize = () => {
-      const canvas = renderer?.domElement;
-      const width = containerRef.current?.clientWidth || 0;
-      const height = containerRef.current?.clientHeight || 0;
-      const pixelRatio = window.devicePixelRatio;
-
-      if (canvas) {
-        const canvasPixelWidth = width * pixelRatio;
-        const canvasPixelHeight = height * pixelRatio;
-
-        const needResize = canvas.width !== canvasPixelWidth || canvas.height !== canvasPixelHeight;
-        if (needResize) {
-          renderer.setSize(width, height, false);
-          if (camera) {
-            camera.aspect = width / height;
-            camera.updateProjectionMatrix();
-          }
-        }
+      if (rendererRef.current && containerRef.current) {
+        containerRef.current.appendChild(rendererRef.current.domElement);
       }
     };
 
     const loadIcons = () => {
+      const radius = 30; // Radius of the orbit
+      const angleIncrement = (2 * Math.PI) / techStack.length; // Angle between each icon
+
       techStack.forEach((tech, index) => {
         const loader = new THREE.TextureLoader();
         loader.load(tech.iconUrl, (texture) => {
           const aspect = texture.image.width / texture.image.height;
-          const iconWidth = 20;
+          const iconWidth = 10;
           const iconHeight = iconWidth / aspect;
 
           const geometry = new THREE.PlaneGeometry(iconWidth, iconHeight);
@@ -91,23 +56,14 @@ const ThreeJSStack: React.FC<ThreeJSStackProps> = ({ title, techStack }) => {
             metalness: 0.9,
           });
 
+          const angle = index * angleIncrement;
+          const x = Math.cos(angle) * radius;
+          const z = Math.sin(angle) * radius;
+
           const icon = new THREE.Mesh(geometry, material);
-          icon.position.x = (index - (techStack.length - 1) / 2) * iconWidth * 1.8;
-          scene?.add(icon);
+          icon.position.set(x, 0, z);
+          sceneRef.current?.add(icon);
           iconsRef.current.push(icon);
-
-          const spotLight = new THREE.SpotLight(0xffffff, 5);
-          spotLight.position.set(icon.position.x, 50, 50);
-          spotLight.angle = Math.PI / 4;
-          spotLight.penumbra = 0.1;
-          spotLight.decay = 2;
-          spotLight.distance = 200;
-          spotLight.target = icon;
-          scene?.add(spotLight);
-
-          const pointLight = new THREE.PointLight(0xffffff, 2, 100);
-          pointLight.position.set(icon.position.x, 0, 20);
-          scene?.add(pointLight);
         });
       });
     };
@@ -116,20 +72,46 @@ const ThreeJSStack: React.FC<ThreeJSStackProps> = ({ title, techStack }) => {
       animationId = requestAnimationFrame(animate);
       const time = Date.now() * 0.001;
       iconsRef.current.forEach((icon, index) => {
-        icon.rotation.y += 0.008;
-        icon.position.y = Math.sin(time + index) * 2;
+        icon.rotation.y += 0.008; // Rotate around its own axis
+        const radius = 30; 
+        const speed = 0.5; 
+        const angle = time * speed + (index / techStack.length) * (2 * Math.PI); // Adjusted angle based on index
+
+        const x = Math.cos(angle) * radius;
+        const z = Math.sin(angle) * radius;
+        icon.position.set(x, 0, z);
       });
-      renderer?.render(scene as THREE.Scene, camera as THREE.Camera);
+      rendererRef.current?.render(sceneRef.current as THREE.Scene, cameraRef.current as THREE.Camera);
+    };
+
+    const resizeRendererToDisplaySize = () => {
+      const renderer = rendererRef.current;
+      const container = containerRef.current;
+      if (renderer && container) {
+        const width = container.clientWidth;
+        const height = container.clientHeight;
+        const pixelRatio = window.devicePixelRatio;
+
+        const canvasPixelWidth = width * pixelRatio;
+        const canvasPixelHeight = height * pixelRatio;
+
+        const needResize = renderer.domElement.width !== canvasPixelWidth || renderer.domElement.height !== canvasPixelHeight;
+        if (needResize) {
+          renderer.setSize(width, height, false);
+          cameraRef.current.aspect = width / height;
+          cameraRef.current.updateProjectionMatrix();
+        }
+      }
     };
 
     const cleanup = () => {
-      cancelAnimationFrame(animationId);
-      if (renderer && renderer.domElement.parentNode) {
-        renderer.domElement.parentNode.removeChild(renderer.domElement);
+      cancelAnimationFrame(animationId as number);
+      if (rendererRef.current && rendererRef.current.domElement.parentNode) {
+        rendererRef.current.domElement.parentNode.removeChild(rendererRef.current.domElement);
       }
-    
-      renderer?.domElement.removeEventListener('webglcontextlost', handleContextLost);
-      renderer?.domElement.removeEventListener('webglcontextrestored', handleContextRestored);
+      rendererRef.current = null;
+      sceneRef.current = null;
+      cameraRef.current = null;
     };
 
     initializeScene();
@@ -142,16 +124,12 @@ const ThreeJSStack: React.FC<ThreeJSStackProps> = ({ title, techStack }) => {
     return () => {
       cleanup();
       window.removeEventListener('resize', resizeRendererToDisplaySize);
-
-      scene = null;
-      camera = null;
-      renderer = null;
     };
   }, [techStack]);
 
   return (
     <div>
-      <div ref={containerRef} className="w-full h-72" />
+      <div ref={containerRef} className="w-full h-80" />
       <h1 className="flex flex-col justify-center items-center font-bold">{title}</h1>
     </div>
   );
