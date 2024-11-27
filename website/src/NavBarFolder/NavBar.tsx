34350bf45604d7c6cacb67c29db6c07a67a86bfe
Rasmus <rasmustofthermansen@gmail.com>
2024-11-27 14:15:01 +0100

    Added new icon for mobile phones

--- website/src/NavBarFolder/NavBar.tsx
+++ website/src/NavBarFolder/NavBar.tsx
@@ -1,26 +1,29 @@
 import { useState } from "react";
 import { decideID } from "../scroll";
-
+import burgermenu from '../assets/whiteBurger.png'
+import xIcon from '../assets/whiteX.png'
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
-
+ const myIcon = "";
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+    
+    
   };
 
   return (
     <div className="font-black font-serif text-indigo-600">
       <div className="fixed text-2xl top-0 left-0 md:hidden w-full z-50">
-        <div className="flex  justify-center items-center px-4 py-2">
+        <div className="flex  justify-center items-center ">
           <button
-            className="h-10 px-6 m-2 text-lg text-indigo-100 z-50 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+            className=" px-6 pt-10 m-2 text-lg text-indigo-100 z-50 transition-colors duration-150 bg-transparent rounded-lg focus:shadow-outline "
             onClick={toggleMenu}
           >
-            ☰
+            <img className="mt-5" src={isOpen ? xIcon : burgermenu} />
           </button>
         </div>
 
-        <div className={`flex-col px-4 pb-4  ${isOpen ? "flex" : "hidden"}`}>
+        <div className={`flex-col  px-4 pb-4  ${isOpen ? "flex" : "hidden"}`}>
           <button
             className="w-full py-2 hover:text-pink-300 "
             onClick={() => decideID("about")}
