f0772b0c32ce2f165c0c72097a6b6a1617ffb395
Darpzz <rasmustofthermansen@gmail.com>
2025-02-09 03:18:30 +0100

    Changed the about to match my current status and age

--- website/src/AboutFolder/About.tsx
+++ website/src/AboutFolder/About.tsx
@@ -12,21 +12,26 @@ function About() {
           <div className="text-lg font-Alegreya font-semibold">
             <Reveal>
               <p>
-              My name is Rasmus, and I'm a 23-year-old junior studying computer science in Denmark.
-              I’m deeply motivated by a continuous curiosity and a genuine passion for exploring
-              complex technologies and tackling challenging problems.
+                {" "}
+                My name is Rasmus, and I'm a 24-year-old recent computer science
+                graduate from Denmark. I’m deeply motivated by a continuous
+                curiosity and a genuine passion for exploring complex
+                technologies and tackling challenging problems.{" "}
               </p>
             </Reveal>
             <br />
             <Reveal>
               <p>
-              I find excitement in understanding the intricacies of how things work and applying this knowledge to create efficient solutions.
+                I find excitement in understanding the intricacies of how things
+                work and applying this knowledge to create efficient solutions.
               </p>
             </Reveal>
             <br />
             <Reveal>
               <p>
-              My studies and projects allow me to develop and refine my skills, equipping me to approach real-world problems with a thoughtful, technical perspective.
+                My studies and projects allow me to develop and refine my
+                skills, equipping me to approach real-world problems with a
+                thoughtful, technical perspective.
               </p>
             </Reveal>
           </div>
