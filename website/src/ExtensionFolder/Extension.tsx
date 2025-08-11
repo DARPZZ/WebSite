04982eeb05dcdb7441574545ca0179ace33ba5bc
Darpzz <rasmustofthermansen@gmail.com>
2025-08-12 00:05:17 +0200

    Add linkedin count to extensions path

--- website/src/ExtensionFolder/Extension.tsx
+++ website/src/ExtensionFolder/Extension.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ExtensionCard from "./ExtensionCard";
-import Geo from "../assets/Geo.png";
-import PrivateInstergramPic from "../assets/PrivateInstergramPic.png";
 import AdBlockerPic from "../assets/AdBlockerPic.png";
+import Geo from "../assets/Geo.png";
+import LinkedinPic from "../assets/LinkedinPic.png";
+import PrivateInstergramPic from "../assets/PrivateInstergramPic.png";
+import ExtensionCard from "./ExtensionCard";
 function Extension() {
   const startGithub = "https://github.com/DARPZZ/";
   //#region ExTENSIONS REGION
@@ -27,19 +27,28 @@ function Extension() {
     name: "Ad Remover",
     img: AdBlockerPic,
   };
+  const LinkedinNumbers = {
+    description:"This browser extension allows you to view the actual number of applications a job has received on LinkedIn, giving you more accurate insight into the level of competition for that position.",
+    name: "Linkedin count",
+    img: LinkedinPic,
+    seeCode: `${startGithub}Linkedin-real-applies-numbers-extension`,
+    downLoadInstruction: `${startGithub}Linkedin-real-applies-numbers-extension/blob/main/README.md`,
+  }
   //#endregion
   return (
     <div className="pt-10">
       <div className="text-green-400 text-3xl justify-center flex font-bold">
-        <h1 className="text-4xl md:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-emerald-400 to-teal-400 animate-fade-in-up text-center">
+        <h1 className="text-4xl md:text-5xl mb-10 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-emerald-400 to-teal-400 animate-fade-in-up text-center">
           Take a Look at Some Chrome Extensions I've Built
         </h1>
       </div>
-      <div className=" mt-12 md:mt-0 md:h-screen flex justify-center items-center">
+      <div className=" mt-12 md:mt-0  flex justify-center items-center">
         <div className=" gap-12 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 grid items-stretch">
           <ExtensionCard project={GeoGessur}></ExtensionCard>
           <ExtensionCard project={Instergram}></ExtensionCard>
+          <ExtensionCard project={LinkedinNumbers}></ExtensionCard>
           <ExtensionCard project={AdRemover}></ExtensionCard>
+          
         </div>
       </div>
     </div>
